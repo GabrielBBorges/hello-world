9cba77379fccabec7166ec4ececb56e3f75470ca
Gabriel Borges <gabrielborgesarilho@gmail.com>
2021-04-12 18:10:19 -0300

    Importando o projeto aqui

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+DESAFIO :]
+function adicionarUmTrailer(){
+	var trailerAdicionado = document.querySelector('#campoTrailer') //a pessoa vai escrever o link e essa variavel vai receber ela
+	trailerValor = trailerAdicionado.value
+	console.log(trailerValor) //E o valor vai ser exibido nesse trailerValor
+	listarImagem(trailerValor)
+	}
+function listarImagem(imagem){
+	var imagemAqui = document.querySelector('#imagemAqui')
+	var elementoImg = "<img src="+imagem+">"
+	var imagemAqui.innerHTML = imagemAqui.innerHTML + elementoImg
+}
+*/
